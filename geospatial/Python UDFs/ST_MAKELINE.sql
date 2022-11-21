24dd137cad7012c209656145d2621caa7fb41ef4
Oleksii Bielov <105351625+sfc-gh-obielov@users.noreply.github.com>
2022-11-21 14:17:21 +0100

    added code for aggregated function for st_makeline

---
+++ geospatial/Python UDFs/ST_MAKELINE.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION PY_MAKELINE(g1 array)
+returns geography
+language python
+runtime_version = 3.8
+packages = ('geopandas','shapely', 'numpy==1.22.3')
+handler = 'udf'
+AS $$
+import geopandas as gpd
+from shapely.geometry import Point, LineString
+from shapely.geometry import shape, mapping
+def udf(g1):
+    geo_object = gpd.GeoSeries([shape(i) for i in g1])
+    shape_union = LineString(geo_object.tolist())
+    return mapping(shape_union)
+$$;
+
+SELECT PY_MAKELINE((ARRAY_AGG(st_asgeojson(geo))) FROM OBJ_OF_INTEREST_SO;
