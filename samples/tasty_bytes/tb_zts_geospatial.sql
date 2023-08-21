b2d816a0739019d19bffd6226bd9e9bc4db50330
Jacob Kranzler <jacob.kranzler@snowflake.com>
2023-08-21 12:57:41 -0600

    Update tb_zts_geospatial.sql
    adjusting USE ROLE sysdmin; to USE ROLE sysadmin;

--- samples/tasty_bytes/tb_zts_geospatial.sql
+++ samples/tasty_bytes/tb_zts_geospatial.sql
@@ -237,7 +237,7 @@ ORDER BY kilometer_from_top_selling_center DESC;
 
 UNSET center_point;
 
-USE ROLE sysdmin;
+USE ROLE sysadmin;
 
 CREATE OR REPLACE VIEW frostbyte_tasty_bytes.analytics.orders_v
 COMMENT = 'Tasty Bytes Order Detail View'
