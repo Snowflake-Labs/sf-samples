61a152340201c19de97dfbace030bf17468fb42d
Oleksii Bielov <105351625+sfc-gh-obielov@users.noreply.github.com>
2023-12-04 11:03:31 +0100

    Update JS_POLYGON_TO_LINESTRING.sql

--- samples/geospatial/Python UDFs/JS_POLYGON_TO_LINESTRING.sql
+++ samples/geospatial/Python UDFs/JS_POLYGON_TO_LINESTRING.sql
@@ -1,4 +1,4 @@
--- Converts the polygon into linestring
+-- Converts the GEOGRAPHY polygon into linestring
 
 CREATE OR REPLACE FUNCTION ST_POLYGON_TO_LINESTRING(G GEOGRAPHY)
 RETURNS OBJECT
@@ -22,6 +22,28 @@ $$;
 -- How to use:
 SELECT ST_POLYGON_TO_LINESTRING(TO_GEOGRAPHY('{"type":"Polygon","coordinates":[[[-180,-72],[180,-72],[180,72],[-180,72],[-180,-72]]]}')) as poly_oriented_area;
 
-
 SELECT st_area(ST_MAKEPOLYGONORIENTED(TO_GEOGRAPHY(ST_POLYGON_TO_LINESTRING(TO_GEOGRAPHY('{"type":"Polygon","coordinates":[[[-180,-72],[180,-72],[180,72],[-180,72],[-180,-72]]]}'))))) as poly_oriented_area, 
 st_area(TO_GEOGRAPHY('{"type":"Polygon","coordinates":[[[-180,-72],[180,-72],[180,72],[-180,72],[-180,-72]]]}')) as poly_area;
+
+
+-- Another option that takes VARIANT as an input
+CREATE OR REPLACE FUNCTION ST_POLYGON_TO_LINESTRING(G OBJECT)
+RETURNS OBJECT
+LANGUAGE JAVASCRIPT
+AS $$
+    if (G.type !== "Polygon") {
+        throw "Input must be a Polygon";
+    }
+
+    // Get the first (outer) ring of the polygon
+    let outerRing = G.coordinates[0];
+    let lineString = {
+        "type": "LineString",
+        "coordinates": outerRing
+    };
+
+    return lineString;
+$$;
+
+-- How to use:
+SELECT st_area(ST_MAKEPOLYGONORIENTED(TO_GEOGRAPHY(ST_POLYGON_TO_LINESTRING('{"type":"Polygon","coordinates":[[[-180,-72],[180,-72],[180,72],[-180,72],[-180,-72]]]}'::VARIANT))));
