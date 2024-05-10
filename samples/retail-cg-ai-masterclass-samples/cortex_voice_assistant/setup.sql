c78c123728d90e1e179c8fa55273d0b2416ee89d
Val Fontama <val.fontama@snowflake.com>
2024-05-10 18:41:34 -0500

    Update setup.sql

--- samples/retail-cg-ai-masterclass-samples/cortex_voice_assistant/setup.sql
+++ samples/retail-cg-ai-masterclass-samples/cortex_voice_assistant/setup.sql
@@ -16,3 +16,23 @@ CREATE STAGE IF NOT EXISTS ML_HOL_DB.ML_HOL_SCHEMA.DIAMONDS_ASSETS
     -- https://sfquickstarts.s3.us-west-1.amazonaws.com/intro-to-machine-learning-with-snowpark-ml-for-python/diamonds.csv
 
 LS @DIAMONDS_ASSETS;
+
+CREATE TABLE IF NOT EXISTS DIAMONDS (
+    CARAT   NUMBER(10,3),
+    CUT     VARCHAR,
+    COLOR   VARCHAR,
+    CLARITY VARCHAR,
+    DEPTH   NUMBER(10,2),
+    TABLE_PCT   NUMBER(10,2),
+    PRICE   NUMBER(20),    
+    X       NUMBER(10,2),
+    Y       NUMBER(10,2),
+    Z       NUMBER(10,2)
+);
+
+COPY INTO DIAMONDS
+  FROM @DIAMONDS_ASSETS
+  FILE_FORMAT = (FORMAT_NAME = 'CSVFORMAT')
+  ON_ERROR = CONTINUE;  
+    
+SELECT * FROM DIAMONDS LIMIT 100; 
