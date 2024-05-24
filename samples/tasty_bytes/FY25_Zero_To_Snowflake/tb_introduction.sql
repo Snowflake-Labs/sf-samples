053c7b07313642ae67518127e1c506ccd9f37d8f
Jacob Kranzler <jacob.kranzler@snowflake.com>
2024-05-24 14:11:56 -0600

    Update tb_introduction.sql

--- samples/tasty_bytes/FY25_Zero_To_Snowflake/tb_introduction.sql
+++ samples/tasty_bytes/FY25_Zero_To_Snowflake/tb_introduction.sql
@@ -8,8 +8,7 @@
                           __/ |          __/ |               
                          |___/          |___/            
 Quickstart:   Tasty Bytes - Zero to Snowflake - Introduction
-Version:      v1
-Script:       tb_fy25_introduction.sql         
+Version:      v1     
 Author:       Jacob Kranzler
 Copyright(c): 2024 Snowflake Inc. All rights reserved.
 ****************************************************************************************************
